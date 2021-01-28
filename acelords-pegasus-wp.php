64688fe2fd232a38f27d78d807de17fb24159854
Lexx YungCarter <spencermainaa@gmail.com>
2021-01-28 17:24:22 +0300

    RC v1.3.2

--- acelords-pegasus-wp.php
+++ acelords-pegasus-wp.php
@@ -8,7 +8,7 @@
  * Author URI: https://www.acelords.space
  */
 
-define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.3.1');
+define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.3.2');
 
 /**
  * The constructor, to initiate the widget
