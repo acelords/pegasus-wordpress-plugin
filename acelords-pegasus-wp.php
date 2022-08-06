133417a5153a56a3c05513b5c3b20380d19bff93
Lexx YungCarter <spencermainaa@gmail.com>
2022-08-06 03:25:51 +0300

    RC v1.6.4

--- acelords-pegasus-wp.php
+++ acelords-pegasus-wp.php
@@ -3,12 +3,12 @@
  * Plugin Name: AceLords Project Pegasus WordPress Plugins
  * Plugin URI: https://github.com/acelords/pegasus-wordpress-plugin
  * Description: WordPress Plugins for complementing AceLords' Project Pegasus
- * Version: 1.6.3
+ * Version: 1.6.4
  * Author: AceLords
  * Author URI: https://www.acelords.space
  */
 
-define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.6.3');
+define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.6.4');
 
 /**
  * The constructor, to initiate the widget
@@ -34,7 +34,7 @@ function handle_shortcode($atts = '') {
     if(! $setValue || $setValue == "") {
         $setValue = "https://app." . $_SERVER['SERVER_NAME'];
     }
-    
+
     return '
         <script> var ACELORDS_PEGASUS_SYSTEM_URL = "' . $setValue . '";</script>
         <div id="acelords-pegasus-plugin-' . $name . '"></div>
@@ -46,7 +46,7 @@ add_shortcode('AceLordsPegasusPlugins', 'handle_shortcode');
 function acelords_pegasus_plugins_enqueue_scripts() {
     global $post;
     if(has_shortcode($post->post_content, "AceLordsPegasusPlugins")) {
-        
+
         wp_enqueue_script( 'acelords-pegasus-plugin-js-manifest', plugin_dir_url( __FILE__ ) . 'public/js/manifest.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
         wp_enqueue_script( 'acelords-pegasus-plugin-js-vendor', plugin_dir_url( __FILE__ ) . 'public/js/vendor.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
         wp_enqueue_script( 'acelords-pegasus-plugin-js-app', plugin_dir_url( __FILE__ ) . 'public/js/app.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
@@ -70,7 +70,7 @@ if( ! function_exists("acelords_pegasus_wordpress_plugins_page") ) {
                 <p class="tw-text-center font-raleway">View and manage settings for AceLords plugins.  (v<?php echo ACELORDS_PEGASUS_WP_PLUGINS_VERSION ?>)</p>
             </div>
 
-            <form method="POST" action="options.php" class="tw-flex tw-flex-wrap"> 
+            <form method="POST" action="options.php" class="tw-flex tw-flex-wrap">
                 <div class="">
                     <?php settings_fields( 'pegasus_setting_field' ); ?>
                 </div>
@@ -83,7 +83,7 @@ if( ! function_exists("acelords_pegasus_wordpress_plugins_page") ) {
             </form>
 
             <div id="acelords-pegasus-wordpress-plugins-admin"></div>
-            
+
         <?php
     }
 
@@ -97,7 +97,7 @@ if( ! function_exists("acelords_pegasus_wordpress_plugins_page") ) {
             'pegasus_setting_section_callback_function',
             'pegasus_setting_field'
         );
-    
+
         add_settings_field(
             'pegasus_setting_field',
             __( 'Pegasus System URL', 'pegasus-wordpress' ),
@@ -108,11 +108,11 @@ if( ! function_exists("acelords_pegasus_wordpress_plugins_page") ) {
 
         register_setting( 'pegasus_setting_field', 'pegasus_setting_field' );
     }
-    
+
     function pegasus_setting_section_callback_function() {
         echo '<p>Set the URL in which the plugins shall fetch their data.</p>';
     }
-    
+
     function pegasus_setting_markup() {
         $setValue = get_option( 'pegasus_setting_field' );
         if(! $setValue || $setValue == "") {
@@ -151,7 +151,7 @@ if( ! function_exists("acelords_pegasus_wordpress_plugins_page") ) {
             wp_enqueue_script( 'acelords-pegasus-plugin-js-manifest', plugin_dir_url( __FILE__ ) . 'public/js/manifest.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
             wp_enqueue_script( 'acelords-pegasus-plugin-js-vendor', plugin_dir_url( __FILE__ ) . 'public/js/vendor.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
             wp_enqueue_script( 'acelords-pegasus-plugin-js-admin-app', plugin_dir_url( __FILE__ ) . 'public/js/admin-app.js', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION, true);
-    
+
             wp_enqueue_style( 'acelords-pegasus-plugin-app-css', plugin_dir_url( __FILE__ ) . 'public/css/app.css', [], ACELORDS_PEGASUS_WP_PLUGINS_VERSION );
             wp_enqueue_style( 'tailwind-css', plugin_dir_url( __FILE__ ) . 'public/css/tailwind.css', [], '1.9.5' );
             wp_enqueue_style( 'mdi-fonts-css', plugin_dir_url( __FILE__ ) . 'public/fonts/mdi/css/materialdesignicons.min.css', [], '5.0.45' );
