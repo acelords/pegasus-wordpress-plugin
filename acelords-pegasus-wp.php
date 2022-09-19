a000e30e30a185df7320b3f12ff11f8dd479d15d
Lexx YungCarter <spencermainaa@gmail.com>
2022-09-19 18:06:58 +0300

    RC v1.6.9

--- acelords-pegasus-wp.php
+++ acelords-pegasus-wp.php
@@ -3,12 +3,12 @@
  * Plugin Name: AceLords Project Pegasus WordPress Plugins
  * Plugin URI: https://github.com/acelords/pegasus-wordpress-plugin
  * Description: WordPress Plugins for complementing AceLords' Project Pegasus
- * Version: 1.6.8
+ * Version: 1.6.9
  * Author: AceLords
  * Author URI: https://www.acelords.space
  */
 
-define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.6.8');
+define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.6.9');
 
 /**
  * The constructor, to initiate the widget
