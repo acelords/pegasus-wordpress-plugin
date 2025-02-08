f9910006b1396476faa225a7a243babb09109a45
SyntaxLexx <spencermainaa@gmail.com>
2025-02-08 12:57:46 +0300

    RC v1.7.1

--- acelords-pegasus-wp.php
+++ acelords-pegasus-wp.php
@@ -3,12 +3,12 @@
  * Plugin Name: AceLords Project Pegasus WordPress Plugins
  * Plugin URI: https://github.com/acelords/pegasus-wordpress-plugin
  * Description: WordPress Plugins for complementing AceLords' Project Pegasus
- * Version: 1.7.0
+ * Version: 1.7.1
  * Author: AceLords
  * Author URI: https://www.acelords.space
  */
 
-define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.7.0');
+define('ACELORDS_PEGASUS_WP_PLUGINS_VERSION', '1.7.1');
 
 /**
  * The constructor, to initiate the widget
